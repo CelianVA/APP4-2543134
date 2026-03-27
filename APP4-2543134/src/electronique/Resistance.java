0c205ad2b3bf53f2dc442a4dcb748b1ef7712004
2543134 <2543134@cegeplimoilou.ca>
2026-03-27 12:50:39 -0400

    Creation de resistance

--- APP4-2543134/src/electronique/Resistance.java
+++ APP4-2543134/src/electronique/Resistance.java
@@ -1,4 +1,29 @@
 package electronique;
 
 public class Resistance extends Composant {
+   private double   resistance ;
+
+    public Resistance(double resistance) {
+        setResistance( resistance);
+    }
+
+    public double getResistance() {
+        return resistance;
+    }
+
+    public void setResistance(double resistance) {
+        if (isResistanceValid(resistance)){
+            this.resistance = resistance ;
+        }
+        else throw new IllegalArgumentException("la resistance n'est pas valide ");
+    }
+    public boolean isResistanceValid(double resistance) {
+        if (resistance > 0 )return true;
+        else return false ;
+    }
+
+    @Override
+    public double calculerResistance() {
+        return getResistance();
+    }
 }
