67a36be5a7c2b9879bcb2d3520215cbe187f09d0
2543134 <2543134@cegeplimoilou.ca>
2026-03-31 10:33:50 -0400

    Implementation de CircuitSerie

--- APP4-2543134/src/electronique/CircuitSerie.java
+++ APP4-2543134/src/electronique/CircuitSerie.java
@@ -1,4 +1,36 @@
 package electronique;
 
+import java.util.List;
+
 public class CircuitSerie extends Circuit{
+    private List<Composant> composants;
+
+    public CircuitSerie(List<Composant> composants) {
+        setResistance(composants);
+    }
+
+    public List<Composant> getResistance() {
+        return  composants;
+    }
+
+    public void setResistance(List<Composant> composants) {
+        this.composants = composants ;
+
+
+        }
+
+    public boolean isResistanceValid(double resistance) {
+        if (resistance > 0 )return true;
+        else return false ;
+    }
+
+
+    @Override
+    public double calculerResistance() {
+        double resistance = 0;
+        for (int i = 0; i < composants.size(); i++) {
+           resistance += composants.get(i).calculerResistance();
+        }
+        return resistance;
+    }
 }
