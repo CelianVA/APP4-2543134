53868cb3245eb88b94fc75fb5e6aecd8ee3b58ed
2543134 <2543134@cegeplimoilou.ca>
2026-03-31 10:41:26 -0400

    Implementation de CircuitParallele

--- APP4-2543134/src/electronique/CircuitParallele.java
+++ APP4-2543134/src/electronique/CircuitParallele.java
@@ -1,4 +1,36 @@
 package electronique;
 
-public class CircuitParallele extends Circuit {
+import java.util.List;
+
+public class CircuitParallele extends Circuit{
+    private List<Composant> composants;
+
+    public CircuitParallele(List<Composant> composants) {
+        setResistance(composants);
+    }
+
+    public List<Composant> getResistance() {
+        return  composants;
+    }
+
+    public void setResistance(List<Composant> composants) {
+        this.composants = composants ;
+
+
+    }
+
+    public boolean isResistanceValid(double resistance) {
+        if (resistance > 0 )return true;
+        else return false ;
+    }
+
+
+    @Override
+    public double calculerResistance() {
+        double resistance = 0;
+        for (int i = 0; i < composants.size(); i++) {
+            resistance += 1/composants.get(i).calculerResistance();
+        }
+        return 1/resistance;
+    }
 }
