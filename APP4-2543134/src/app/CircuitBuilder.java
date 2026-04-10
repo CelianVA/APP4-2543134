9d66858d1803dfb34a80ae3bf4b81d04f9fb453b
CelianVA <2543134@cegeplimoilou.ca>
2026-04-10 11:56:50 -0400

    Finition de CircuitBuilder

--- APP4-2543134/src/app/CircuitBuilder.java
+++ APP4-2543134/src/app/CircuitBuilder.java
@@ -1,4 +1,65 @@
 package app;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import electronique.CircuitSerie;
+import electronique.Composant;
+import electronique.Resistance;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CircuitBuilder {
-}
+    public CircuitBuilder() {
+
+    }
+
+
+    public Composant constuireCircuit(String pathname) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.readTree(new File(pathname ));
+        return lireComposant(node);
+    }
+    private Composant lireComposant(JsonNode node) throws IOException {
+        String type = node.get("circuit").get("type").asText();
+        switch (type){
+            case "resistance":
+                return new Resistance(node.get("circuit").get("valeur").asDouble());
+            case "serie":
+                List<Composant> composantsSerie = new ArrayList<>();
+                for(JsonNode b : node.get("circuit").get("composants")){
+
+                    composantsSerie.add(lireComposant(b));
+                }
+                return new CircuitSerie(composantsSerie);
+
+
+
+            case "parallele":
+                List<Composant> composantsParallele = new ArrayList<>();
+                for(JsonNode b : node.get("circuit").get("composants")){
+
+                    composantsParallele.add(lireComposant(b));
+                }
+                return new CircuitSerie(composantsParallele);
+
+
+
+        }
+        throw new IllegalArgumentException();
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
