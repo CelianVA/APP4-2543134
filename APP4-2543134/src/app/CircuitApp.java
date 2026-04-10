3c88504d48332a490ca19c0b05efdac4f114bbb6
CelianVA <2543134@cegeplimoilou.ca>
2026-04-10 11:57:12 -0400

    Finition de CircuitApp

--- APP4-2543134/src/app/CircuitApp.java
+++ APP4-2543134/src/app/CircuitApp.java
@@ -1,4 +1,23 @@
 package app;
 
+import electronique.Circuit;
+import electronique.Composant;
+
+import java.io.File;
+import java.io.IOException;
+
 public class CircuitApp {
+    public static final char fSep = File.separatorChar;
+    public static void main(String[] args) throws IOException {
+        InterfaceUtilisateur n = new InterfaceUtilisateur();
+        String dossierName = n.affichage();
+        String pathname = System.getProperty("user.dir") + fSep + "APP4-2543134" + fSep + "src" + fSep + "donnees" + fSep + dossierName;
+        CircuitBuilder circuitBuilder = new CircuitBuilder();
+
+        Composant circuit = circuitBuilder.constuireCircuit(pathname);
+        Double resitanceTotal = circuit.calculerResistance();
+        System.out.println(resitanceTotal);
+        n.recomencer();
+    }
+
 }
